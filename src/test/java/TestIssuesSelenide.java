6ab4063a67819f35d1076a14210ede8b8b10c8c9
Konyaz <80565760+Konyaz@users.noreply.github.com>
2021-03-30 16:52:20 +0900

    add

--- src/test/java/TestIssuesSelenide.java
+++ src/test/java/TestIssuesSelenide.java
@@ -8,15 +8,27 @@ import static com.codeborne.selenide.Selenide.open;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class testIssues {
-}
+public class TestIssuesSelenide {
+
+    String REPOSITORY = "Konyaz/currant_allure";
+
     @Test
-    void searchForIssue() {
+
+    public void TestIssueSearch() {
         open("https://github.com");
-        $("#.header-search-input").click();
-        $("")
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(REPOSITORY);
+        $(".header-search-input").submit();
+        $(By.linkText(REPOSITORY)).click();
+        $(".UnderlineNav-body").shouldHave(text("Issue"));
+
+    }
+
+}
